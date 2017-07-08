1c3864d92ba2524f14aabf930b393fd711e121f5
MaximFirsov <firsoffmaxim@gmail.com>
2017-07-08 11:36:24 +0500

    L5.1 homework

---
+++ L5.1/src/main/java/ru/otus/l51/framework/annotations/After.java
@@ -0,0 +1,13 @@
+package ru.otus.l51.framework.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by maximfirsov on 07/07/2017.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+@Inherited
+public @interface After {
+
+}
