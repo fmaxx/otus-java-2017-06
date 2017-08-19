6434d57066d9cfc18de229120300106887c366eb
MaximFirsov <firsoffmaxim@gmail.com>
2017-08-19 10:50:18 +0500

    template-L81

---
+++ L8.1/src/main/java/ru/otus/l81/Main.java
@@ -0,0 +1,15 @@
+package ru.otus.l81;
+
+import java.io.IOException;
+
+/**
+ * Created by maximfirsov on 06/07/2017.
+ *
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("--- run classes ---");
+    }
+}
