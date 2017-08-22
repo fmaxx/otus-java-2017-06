71623b6eb60b8bf967750dd196f72a6444e1fe13
Maxim Firsov <firsoffmaxim@gmail.com>
2017-08-22 10:53:05 +0500

    refactoring

--- L8.1/src/main/java/ru/otus/l81/JsonHelper.java
+++ L8.1/src/main/java/ru/otus/l81/JsonHelper.java
@@ -3,7 +3,6 @@ package ru.otus.l81;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
-import javax.json.JsonValue;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,7 +11,7 @@ import java.util.List;
 
 public class JsonHelper {
 
-    public static String toJSONString(Object object){
+    public static String toJSONString(Object object) {
         return convertToJsonObject(object).build().toString();
     }
 
@@ -23,117 +22,63 @@ public class JsonHelper {
 
         for (Field field : fields) {
 
-            JsonValue.ValueType jsonValueType = getJsonValue(field, object);
-            Object fieldValue = null;
+            // VALUE
+            Object value = null;
             try {
-                fieldValue = field.get(object);
+                value = field.get(object);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
 
-            if (fieldValue == null) {
-                jsonValueType = JsonValue.ValueType.NULL;
-            }else if(fieldValue instanceof Collection){
-                jsonValueType = JsonValue.ValueType.ARRAY;
-                fieldValue = ((Collection) fieldValue).toArray();
-            }
+            // TYPE
+            Class type = field.getType();
 
-            switch (jsonValueType) {
-                case NUMBER:
-                    if (fieldValue instanceof Byte) {
-                        builder.add(field.getName(), (byte) fieldValue);
-                    } else if (fieldValue instanceof Integer) {
-                        builder.add(field.getName(), (int) fieldValue);
-                    } else if (fieldValue instanceof Short) {
-                        builder.add(field.getName(), (short) fieldValue);
-                    } else if (fieldValue instanceof Long) {
-                        builder.add(field.getName(), (long) fieldValue);
-                    } else if (fieldValue instanceof Float) {
-                        builder.add(field.getName(), (float) fieldValue);
-                    } else if (fieldValue instanceof Double) {
-                        builder.add(field.getName(), (double) fieldValue);
-                    }
-                    break;
-
-                case OBJECT:
-                    builder.add(field.getName(), convertToJsonObject(fieldValue));
-                    break;
-                case STRING:
-                    if (fieldValue instanceof Character) {
-                        builder.add(field.getName(), (char) fieldValue);
-                    } else {
-                        builder.add(field.getName(), (String) fieldValue);
-                    }
-                    break;
-
-                case FALSE:
-                case TRUE:
-                    builder.add(field.getName(), (boolean) fieldValue);
-                    break;
-
-                case NULL:
-                    builder.addNull(field.getName());
-                    break;
-
-                case ARRAY:
-                    JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
-                    Object[] test = (Object[]) fieldValue;
-                    for (Object o : test) {
-                        arrayBuilder.add(convertToJsonObject(o));
-                    }
-                    builder.add(field.getName(), arrayBuilder);
-                    break;
+            if (value == null) {
+                builder.addNull(field.getName());
+            } else if (type.isPrimitive()) {
+                // PRIMITIVES
+                if (value instanceof Byte) {
+                    builder.add(field.getName(), (byte) value);
+                } else if (value instanceof Integer) {
+                    builder.add(field.getName(), (int) value);
+                } else if (value instanceof Short) {
+                    builder.add(field.getName(), (short) value);
+                } else if (value instanceof Long) {
+                    builder.add(field.getName(), (long) value);
+                } else if (value instanceof Float) {
+                    builder.add(field.getName(), (float) value);
+                } else if (value instanceof Double) {
+                    builder.add(field.getName(), (double) value);
+                } else if (value instanceof Boolean) {
+                    builder.add(field.getName(), (boolean) value);
+                } else if (value instanceof Character) {
+                    builder.add(field.getName(), (char) value);
+                }
+            } else if (value instanceof String) {
+                // STRINGS
+                builder.add(field.getName(), (String) value);
+            } else if (type.isArray()) {
+                JsonArrayBuilder arrayBuilder = convertArray((Object[]) value);
+                builder.add(field.getName(), arrayBuilder);
+            } else if (Collection.class.isAssignableFrom(type)) {
+                // COLLECTIONS
+                value = ((Collection) value).toArray();
+                JsonArrayBuilder arrayBuilder = convertArray((Object[]) value);
+                builder.add(field.getName(), arrayBuilder);
+            } else if (value instanceof Object) {
+                // OBJECTS
+                builder.add(field.getName(), convertToJsonObject(value));
             }
         }
         return builder;
     }
 
-
-    static Boolean isPrimitiveType(Field field, Object object) throws IllegalAccessException {
-        Object fieldValue = field.get(object);
-        return fieldValue.getClass().isPrimitive() ||
-                fieldValue instanceof Byte ||
-                fieldValue instanceof Short ||
-                fieldValue instanceof Integer ||
-                fieldValue instanceof Long ||
-                fieldValue instanceof Float ||
-                fieldValue instanceof Double ||
-                fieldValue instanceof Character ||
-                fieldValue instanceof Boolean;
-    }
-
-    static JsonValue.ValueType getJsonValue(Field field, Object object) {
-        try {
-            if (isArray(field, object)) {
-                return JsonValue.ValueType.ARRAY;
-            } else {
-                Object fieldValue = field.get(object);
-                if (isPrimitiveType(field, object)) {
-                    if (fieldValue instanceof Boolean) {
-                        return (Boolean) fieldValue ? JsonValue.ValueType.TRUE : JsonValue.ValueType.FALSE;
-                    } else if (fieldValue instanceof Character) {
-                        return JsonValue.ValueType.STRING;
-                    }
-                    return JsonValue.ValueType.NUMBER;
-                } else {
-                    if (fieldValue instanceof String) {
-                        return JsonValue.ValueType.STRING;
-                    } else {
-                        return JsonValue.ValueType.OBJECT;
-                    }
-
-                }
-            }
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+    static JsonArrayBuilder convertArray(Object[] array) {
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        for (Object o : array) {
+            arrayBuilder.add(convertToJsonObject(o));
         }
-
-        return JsonValue.ValueType.NULL;
-    }
-
-    static Boolean isArray(Field field, Object object) throws IllegalAccessException {
-        Object fieldValue = field.get(object);
-        return fieldValue.getClass().isArray();
+        return arrayBuilder;
     }
 
     static List<Field> getFields(Class<?> type) {
